e60db0b09e19df8db2455a756e74de33dce6b129
Najrul Islam <ni@hoxro.com>
2022-02-12 08:49:56 +0600

    add dbacpac

--- src/Assessment.DbProject/Script.PostDeployment2.sql
+++ src/Assessment.DbProject/Script.PostDeployment2.sql
@@ -10,9 +10,8 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 	
-	
-	DECLARE @Start_Date DATETIME ='2022-02-01 0:0:0.0';
-	DECLARE @End_Date DATETIME ='2022-02-28 23:59:59.999';
+	DECLARE @Start_Date DATETIME ='2022-02-10 0:0:0.0';
+	DECLARE @End_Date DATETIME ='2022-02-20 23:59:59.999';
 	declare @id int=1;
 	declare @min_integer tinyint =   1
 	,@max_integer tinyint =  20
@@ -20,7 +19,6 @@ Post-Deployment Script Template
 	,@time_stamp_range tinyint =  59
 	,@value_range tinyint =  30
 	,@count int = 1;
-
 		while (@id >=1 and @id <= 100)
 			begin
 				while (@Start_Date < @End_Date)
@@ -31,7 +29,7 @@ Post-Deployment Script Template
 								values (@id, 
 									floor(rand()*(@max_integer - @min_integer + 1) + @min_integer),
 									floor(rand()*(@max_integer - @min_integer + 1) + @min_integer), 
-									floor(rand()*(@value_range - @min_integer + 1) + @min_integer),
+									floor(rand()*(@value_range - 5 + 1) + @min_integer),
 									DATEADD(SECOND, floor(rand()*(@time_stamp_range - @min_integer + 1) + @min_integer), @Start_Date)
 									)
 								select @time_stamp = @time_stamp + 1;
@@ -39,8 +37,10 @@ Post-Deployment Script Template
 						select @time_stamp = 1;
 						set @Start_Date = DATEADD(MI,1, @Start_Date);
 					end
-			set @Start_Date = '2022-02-01 0:0:0.0';
+			set @Start_Date = '2022-02-10 0:0:0.0';
 			select @id = @id + 1;
 			end
 	set @id =1;
 	set @count = 1;
+
+	
